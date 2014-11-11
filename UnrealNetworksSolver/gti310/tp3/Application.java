0e8eff32f14e1dcc9c0d424b60239e116fcb7bf5
Arkonzoa <exoarkonzoa@gmail.com>
2014-11-11 17:19:28 -0500

    Initial Commit

---
+++ UnrealNetworksSolver/gti310/tp3/Application.java
@@ -0,0 +1,26 @@
+package gti310.tp3;
+
+import gti310.tp3.parser.ConcreteParser;
+
+/**
+ * The Application class defines a template method to call the elements to
+ * solve the problem Unreal-Networks is façing.
+ * 
+ * @author François Caron <francois.caron.7@ens.etsmtl.ca>
+ */
+public class Application {
+
+	/**
+	 * The Application's entry point.
+	 * 
+	 * The main method makes a series of calls to find a solution to the
+	 * problem. The program awaits two arguments, the complete path to the
+	 * input file, and the complete path to the output file.
+	 * 
+	 * @param args The array containing the arguments to the files.
+	 */
+	public static void main(String args[]) {
+		Node a = new ConcreteParser().parse("filename");
+		System.out.println("Unreal Networks Solver !");
+	}
+}
